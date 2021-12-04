3c9ef16dfa4361bf6f60fc2c66064414e877c3bc
asiryk <unigenocity@gmail.com>
2021-12-04 22:34:51 +0200

    feat(lualine): add time

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,10 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-  }
+  },
+  sections = {
+    lualine_x = { 'encoding', 'fileformat', 'filetype', 'progress' },
+    lualine_y = { 'os.date("%H:%M")' },
+  },
 }
 
