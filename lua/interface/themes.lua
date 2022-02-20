beed14dda8359970a90398034254ff26fa678517
asiryk <unigenocity@gmail.com>
2022-02-20 22:30:32 +0200

    chore(themes): reformat everforest colorscheme. make everforest be configurable from lua

--- lua/interface/themes.lua
+++ lua/interface/themes.lua
@@ -1,1 +1,23 @@
-vim.cmd[[colorscheme everforest]]
+---@param background? "dark" | "light
+---@param contrast? "hard" | "medium" | "soft"
+---@return string
+local everforest = function(background, contrast)
+  if not background then background = "dark" end
+  if not contrast then contrast = "soft" end
+
+  background = "set background=" .. background .. "\n"
+  contrast = string.format("let g:everforest_background='%s'\n", contrast)
+
+  return background .. contrast .. [[
+    let g:everforest_enable_italic = 1
+    let g:everforest_disable_italic_comment = 1
+
+    if has('termguicolors')
+      set termguicolors
+    endif
+
+    colorscheme everforest
+  ]]
+end
+
+vim.cmd(everforest())
