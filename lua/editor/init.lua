80b94956d57087de25b2590f204fc47fdf21247f
asiryk <unigenocity@gmail.com>
2022-05-06 18:49:00 +0300

    chore: support for svelte; specify file extensions for colorizer

--- lua/editor/init.lua
+++ lua/editor/init.lua
@@ -1,4 +1,6 @@
 require("Comment").setup()
 require "editor/completion"
 require "editor/syntax"
-require "colorizer".setup { "*" } -- requires termguicolors
+require "colorizer".setup {  -- requires termguicolors
+  "html", "css", "javascript", "typescript", "svelte", "lua"
+}
--- lua/editor/syntax.lua
+++ lua/editor/syntax.lua
@@ -26,6 +26,7 @@ require("nvim-treesitter.configs").setup {
     "regex",
     "rust",
     "scala",
+    "svelte",
     "toml",
     "tsx",
     "typescript",
