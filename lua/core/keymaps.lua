38d60fed944dbffaea2482c074343c68ede4a745
asiryk <asiryk.net@gmail.com>
2022-06-30 13:11:32 +0300

    keymap: add switching tabs in insert mode

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,14 +1,14 @@
 local set = vim.keymap.set
 
-set("n", "<bs>", "<nop>")                 -- Unmap Backspace
-set("n", "<space>", "<nop>")              -- Unmap Space
-vim.g.mapleader = " "                     -- Set Leader key
+set("n", "<bs>", "<nop>")                       -- Unmap Backspace
+set("n", "<space>", "<nop>")                    -- Unmap Space
+vim.g.mapleader = " "                           -- Set Leader key
 
-set("n", "tn", "<cmd>tabnew<CR>")         -- Create new tab
-set("n", "<C-L>", "<cmd>tabnext<CR>")     -- Switch to right tab
-set("n", "<C-H>", "<cmd>tabprev<CR>")     -- Switch to left tab
-set("n", "tc", "<cmd>tabclose<CR>")       -- Close current tab
-set("n", "tac", "<cmd>tabo<CR>")          -- Close all tabs
+set("n", "tn", "<cmd>tabnew<CR>")               -- Create new tab
+set({"n", "i"}, "<C-L>", "<cmd>tabnext<CR>")    -- Switch to right tab
+set({"n", "i"}, "<C-H>", "<cmd>tabprev<CR>")    -- Switch to left tab
+set("n", "tc", "<cmd>tabclose<CR>")             -- Close current tab
+set("n", "tac", "<cmd>tabo<CR>")                -- Close all tabs
 
 -- LSP, :help vim.lsp.*
 local buf = vim.lsp.buf
