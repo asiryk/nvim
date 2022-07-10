069a6b5bc0615fe1906ae64bfb57a94f3fdc1522
asiryk <asiryk.net@gmail.com>
2022-07-10 15:05:58 +0300

    chore(cmp): change completion order

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -88,8 +88,8 @@ local options = {
    sources = {
       { name = "luasnip" },
       { name = "nvim_lsp" },
-      { name = "buffer" },
       { name = "nvim_lua" },
+      { name = "buffer" },
       { name = "path" },
    },
 }
