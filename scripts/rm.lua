59de7d587a17beab7eb14c98c76b31a816f8d3f5
asiryk <asiryk.net@gmail.com>
2022-08-23 23:38:10 +0300

    chore: rewrite rm script in lua

---
+++ scripts/rm.lua
@@ -0,0 +1,7 @@
+#!/usr/local/bin/lua
+
+os.execute([[
+rm -rfd ~/.config/nvim/plugin/;
+rm -rfd ~/.local/share/nvim/;
+rm -rfd ~/.cache/nvim/
+]])
--- scripts/rm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-rm -rfd ~/.config/nvim/plugin/
-rm -rfd ~/.local/share/nvim/
-rm -rfd ~/.cache/nvim/
