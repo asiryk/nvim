654f664df6a312458e3de312cec8dd892cfd4ff5
asiryk <asiryk.net@gmail.com>
2022-09-18 18:17:40 +0300

    feat(harpoon): center move

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -15,4 +15,14 @@ function utils.get_buf_size_in_bytes(buf)
   return vim.api.nvim_buf_get_offset(buf, line_count)
 end
 
+-- Wrap given function (assumes that it's move)
+-- and center the screen after calling it.
+-- NOTE: the function shoud be synchrounous
+function utils.center_move(fn)
+  return function()
+    fn()
+    vim.cmd("norm! zz")
+  end
+end
+
 return utils
--- lua/plugins/config/harpoon.lua
+++ lua/plugins/config/harpoon.lua
@@ -2,6 +2,8 @@ local present = pcall(require, "harpoon")
 
 if not present then return end
 
+local bind = require("plenary.fun").bind
+local utils = require("core.utils")
 local harpoon_mark = require("harpoon.mark")
 local harpoon_ui = require("harpoon.ui")
 
@@ -16,7 +18,7 @@ local alt = { -- When I press alt<key> on macos
 
 vim.keymap.set("n", alt["n"], harpoon_mark.add_file)
 vim.keymap.set("n", alt["m"], harpoon_ui.toggle_quick_menu)
-vim.keymap.set("n", alt["j"], function() harpoon_ui.nav_file(1) end)
-vim.keymap.set("n", alt["k"], function() harpoon_ui.nav_file(2) end)
-vim.keymap.set("n", alt["l"], function() harpoon_ui.nav_file(3) end)
-vim.keymap.set("n", alt[";"], function() harpoon_ui.nav_file(4) end)
+vim.keymap.set("n", alt["j"], utils.center_move(bind(harpoon_ui.nav_file, 1)))
+vim.keymap.set("n", alt["k"], utils.center_move(bind(harpoon_ui.nav_file, 2)))
+vim.keymap.set("n", alt["l"], utils.center_move(bind(harpoon_ui.nav_file, 3)))
+vim.keymap.set("n", alt[";"], utils.center_move(bind(harpoon_ui.nav_file, 4)))
