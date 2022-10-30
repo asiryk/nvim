6e7e5ba6cfd5f3fad15605c8b3278fc3df28dd6e
asiryk <asiryk.net@gmail.com>
2022-10-30 15:05:43 +0200

    Add eyliner.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,10 @@ return {
   },
   ["nvim-treesitter/playground"] = { after = "nvim-treesitter" },
 
+  ["jinh0/eyeliner.nvim"] = {
+    config = function() require("eyeliner").setup({ highlight_on_key = true }) end,
+  },
+
   ["mattn/emmet-vim"] = {
     ft = { "html", "svelte" },
     run = ":EmmetInstall",
