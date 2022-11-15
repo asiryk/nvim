474e956c105486aa18a8ebc9c6262df131413ec6
asiryk <asiryk.net@gmail.com>
2022-11-15 22:46:27 +0200

    Rust tools

--- lua/plugins/config/rust-tools.lua
+++ lua/plugins/config/rust-tools.lua
@@ -1,18 +1,35 @@
-local opts = {
-  tools = {
-    -- options same as lsp hover / vim.lsp.util.open_floating_preview()
-    hover_actions = {
-      -- see vim.api.nvim_open_win()
+local rt = require("rust-tools")
 
-      auto_focus = true,
-    },
+rt.setup({
+  -- tools = {
+  --   -- options same as lsp hover / vim.lsp.util.open_floating_preview()
+  --   hover_actions = {
+  --     -- see vim.api.nvim_open_win()
+  --
+  --     auto_focus = true,
+  --   },
+  --
+  --   autoSetHints = true,
+  --   runnables = { use_telescope = true },
+  --   inlay_hints = { show_parameter_hints = true },
+  -- },
+  -- server = {
+  --   on_attach = function(_, buffer)
+  --     -- vim.keymap.set("n", "<C-h>", rt.hover_actions.hover_actions, { buffer = buffer })
+  --     local function set(lhs, rhs) vim.keymap.set("n", lhs, rhs, { buffer = buffer }) end
+  --
+  --     set( "<C-h>", rt.runnables.runnables)
+  --
+  --
+  --     -- default
+  --     set("gd", vim.lsp.buf.definition)
+  --     set("<S-K>", vim.lsp.buf.hover)
+  --     set("<Leader>lf", function() vim.lsp.buf.format({ async = true }) end)
+  --     set("<Leader>lr", vim.lsp.buf.rename)
+  --     set("<Leader>la", vim.lsp.buf.code_action)
+  --     set("<Leader>lcr", vim.lsp.codelens.refresh)
+  --   end,
+  -- },
+})
 
-    autoSetHints = true,
-    runnables = { use_telescope = true },
-    inlay_hints = { show_parameter_hints = true },
-  },
-}
-
-require("rust-tools").setup(opts)
-
-vim.keymap.set("n", "<S-h>", "<cmd>RustHoverActions<cr>")
+-- vim.keymap.set("n", "<S-h>", "<cmd>RustHoverActions<cr>")
