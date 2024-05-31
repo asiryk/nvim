fca462d217402582f787b78a0fb2301ff0dc7cec
asiryk <asiryk.net@gmail.com>
2024-05-31 18:14:10 +0300

    cfg mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,16 @@
 local files = require("mini.files")
 
-vim.keymap.set("n", "<C-n>", files.open, { desc = "Open files [Mini.files]" })
+local function open_current()
+  files.open(vim.api.nvim_buf_get_name(0))
+end
+
+vim.keymap.set("n", "<C-n>", open_current,
+  { desc = "Open current file directory [Mini.files]" }
+)
+vim.keymap.set("n", "<leader>nc", open_current,
+  { desc = "Open current file directory [Mini.files]" }
+)
+
+vim.keymap.set("n", "<leader>no", files.open,
+  { desc = "Open working directory in last used state [Mini.files]" }
+)
