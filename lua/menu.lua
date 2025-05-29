c69e6180b5bf86d5ed47afb9f7b7a4f813a92cc5
asiryk <asiryk.net@gmail.com>
2025-05-29 15:28:13 +0300

    Add Gitsigns to popup menu

--- lua/menu.lua
+++ lua/menu.lua
@@ -9,6 +9,10 @@ vim.cmd([[
   vnoremenu PopUp.Select\ All             gg0oG$
   inoremenu PopUp.Select\ All             <C-Home><C-O>VG
   amenu PopUp.-1-                         <NOP>
+  anoremenu PopUp.?\ No\ Git\ Repo        <cmd>lua vim.notify("Not inside Git repository", vim.log.levels.WARN)<CR>
+  anoremenu PopUp.Git\ Blame              <cmd>Gitsigns blame<CR>
+  anoremenu PopUp.Git\ Blame\ Line        <cmd>Gitsigns blame_line<CR>
+  anoremenu PopUp.Git\ Preview\ Hunk      <cmd>Gitsigns preview_hunk<CR>
   anoremenu PopUp.Git\ Diff\ Open         <cmd>DiffviewOpen<CR>
   anoremenu PopUp.Git\ Diff\ Close        <cmd>DiffviewClose<CR>
   anoremenu PopUp.Git\ Diff\ Preset       <cmd>DiffPreset<CR>
@@ -21,6 +25,8 @@ vim.cmd([[
   "
 ]])
 
+local F = {}
+
 local group = vim.api.nvim_create_augroup("nvim.popupmenu", { clear = true })
 
 vim.api.nvim_create_autocmd("MenuPopup", {
@@ -32,6 +38,20 @@ vim.api.nvim_create_autocmd("MenuPopup", {
     local cword = vim.fn.expand("<cword>")
     local is_git_buf = vim.b.gitsigns_status_dict ~= nil
 
+    local any_git_menu = false
+
+    do -- Git Gitsigns
+      vim.cmd([[amenu disable PopUp.Git\ Blame]])
+      vim.cmd([[amenu disable PopUp.Git\ Blame\ Line]])
+      vim.cmd([[amenu disable PopUp.Git\ Preview\ Hunk]])
+      if is_git_buf then
+        vim.cmd([[amenu enable PopUp.Git\ Blame]])
+        vim.cmd([[amenu enable PopUp.Git\ Blame\ Line]])
+        if F.is_git_hunk() then vim.cmd([[amenu enable PopUp.Git\ Preview\ Hunk]]) end
+        any_git_menu = true
+      end
+    end
+
     do -- Git Diffview
       vim.cmd([[amenu disable PopUp.Git\ Diff\ Open]])
       vim.cmd([[amenu disable PopUp.Git\ Diff\ Close]])
@@ -40,12 +60,15 @@ vim.api.nvim_create_autocmd("MenuPopup", {
       if is_git_buf then
         if require("plugins.diffview").is_diffview_open() then
           vim.cmd([[amenu enable PopUp.Git\ Diff\ Close]])
+          any_git_menu = true
         else
           vim.cmd([[amenu enable PopUp.Git\ Diff\ Open]])
           vim.cmd([[amenu enable PopUp.Git\ Diff\ Preset]])
+          any_git_menu = true
         end
       elseif bufname:match("^diffview:///") then
         vim.cmd([[amenu enable PopUp.Git\ Diff\ Close]])
+        any_git_menu = true
       end
     end
 
@@ -57,8 +80,36 @@ vim.api.nvim_create_autocmd("MenuPopup", {
         if cword:match("^%x+$") then
           vim.cmd([[amenu enable PopUp.Git\ Diff\ Hash]])
           vim.cmd([[amenu enable PopUp.Git\ Copy\ Hash]])
+          any_git_menu = true
         end
       end
     end
+
+    if any_git_menu then
+      vim.cmd([[amenu enable PopUp.?\ No\ Git\ Repo]])
+    else
+      vim.cmd([[amenu disable PopUp.?\ No\ Git\ Repo]])
+    end
   end,
 })
+
+function F.is_git_hunk()
+  local gitsigns = package.loaded.gitsigns
+  if not gitsigns or not gitsigns.get_hunks then return false end
+
+  local hunks = gitsigns.get_hunks()
+  if not hunks then return false end
+
+  local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+
+  for _, hunk in ipairs(hunks) do
+    if
+      cursor_line >= hunk.added.start
+      and cursor_line <= (hunk.added.start + hunk.added.count - 1)
+    then
+      return true
+    end
+  end
+
+  return false
+end
