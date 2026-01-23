d69a1c963272a1d0e2be914a351d79fa08f1e851
asiryk <asiryk.net@gmail.com>
2026-01-23 23:56:45 +0200

    Impl PR shortcut URL for current branch

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -8,6 +8,14 @@ vim.keymap.set(
   { desc = "Commit with amend [Fugitive]" }
 )
 
+vim.keymap.set("n", "<leader>gP", function()
+  local url = L.get_pr_url()
+  if url then
+    -- make that multiline, so it opens :mes history
+    vim.print("\n" .. url)
+  end
+end, { desc = "Print PR URL (GitHub, GitLab) [Fugitive]" })
+
 function F.open_commit_diff_under_cursor()
   pcall(function()
     local line = vim.api.nvim_get_current_line()
@@ -62,6 +70,56 @@ vim.api.nvim_create_user_command("Gitlo", function()
   if output == nil then return end
   L.create_git_graph_buf(output)
 end, { desc = "Git log graph [User]" })
+
+--- Returns a URL to create a new PR/MR for the current branch.
+--- Supports GitHub and GitLab remotes (both SSH and HTTPS formats).
+function L.get_pr_url()
+  local remote = vim.fn.system("git remote get-url origin"):gsub("%s+$", "")
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Failed to get remote origin", vim.log.levels.ERROR)
+    return nil
+  end
+
+  local branch = vim.fn.system("git branch --show-current"):gsub("%s+$", "")
+  if vim.v.shell_error ~= 0 or branch == "" then
+    vim.notify("Failed to get current branch", vim.log.levels.ERROR)
+    return nil
+  end
+
+  -- Normalize SSH URLs to HTTPS format
+  -- git@github.com:user/repo.git -> github.com/user/repo
+  -- git@gitlab.com:group/subgroup/repo.git -> gitlab.com/group/subgroup/repo
+  local host, path
+  if remote:match("^git@") then
+    host, path = remote:match("^git@([^:]+):(.+)$")
+  else
+    host, path = remote:match("^https?://([^/]+)/(.+)$")
+  end
+
+  if not host or not path then
+    vim.notify("Could not parse remote URL: " .. remote, vim.log.levels.ERROR)
+    return nil
+  end
+
+  path = path:gsub("%.git$", "")
+
+  local url
+  if host:match("github") then
+    url = ("https://%s/%s/pull/new/%s"):format(host, path, branch)
+  elseif host:match("gitlab") then
+    local encoded_branch = vim.uri_encode(branch)
+    url = ("https://%s/%s/-/merge_requests/new?merge_request%%5Bsource_branch%%5D=%s"):format(
+      host,
+      path,
+      encoded_branch
+    )
+  else
+    vim.notify("Unknown git host: " .. host, vim.log.levels.WARN)
+    return nil
+  end
+
+  return url
+end
 
 function L.run_git_log()
   local git_cmd = "git log --graph --pretty=format:'%h%d %s <%ad | %an>' --abbrev-commit --date=local"
