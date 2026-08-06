76640665301649b34da9b0b3c54ac6934ecc2444
asiryk <asiryk.net@gmail.com>
2026-08-06 15:00:33 +0300

    Include global functions in treesitter query for js/ts

--- after/ftplugin/javascript/methods.scm
+++ after/ftplugin/javascript/methods.scm
@@ -1,3 +1,4 @@
+; Object literal methods: `foo: function () {}` / `foo: () => {}`
 (pair
   key: (property_identifier) @method_name
   value: (function_expression))
@@ -6,7 +7,23 @@
   key: (property_identifier) @method_name
   value: (arrow_function))
 
+; Class methods and object shorthand methods: `foo() {}`
 (method_definition
   name: (property_identifier) @method_name)
+
+; Top-level (or nested) declarations: `function foo() {}` / `function* foo() {}`
+(function_declaration
+  name: (identifier) @method_name)
+
+(generator_function_declaration
+  name: (identifier) @method_name)
+
+; Function-valued bindings: `const foo = () => {}` / `const foo = function () {}`
+(variable_declarator
+  name: (identifier) @method_name
+  value: [
+    (arrow_function)
+    (function_expression)
+  ])
 
 ; vim: filetype=query
